cee75d7cbd3e1beb137e0f9d25e0d08951f8da6d
Keith D'Atrio <keith@datrio.net>
2023-02-01 10:03:40 -0600

    adding in Pandas for import of configuration

--- poly_dialer/Get-Config.py
+++ poly_dialer/Get-Config.py
@@ -1,24 +1,26 @@
 import os
 from dotenv import load_dotenv
 import requests
+import pandas as pd
+
 
 load_dotenv()
-PhoneUser = os.getenv('PHONEUSER')
-Password = os.getenv('PASSWORD')
-Phone = os.getenv('PHONE')
+PhoneUser = os.getenv("PHONEUSER")
+Password = os.getenv("PASSWORD")
+Phone = os.getenv("PHONE")
 
 headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
 }
 
 json_data = {
-    'data': [
-        'SCEP.enable',
+    "data": [
+        "SCEP.enable",
     ],
 }
 
 response = requests.post(
-    'https://' + Phone + '/api/v1/mgmt/config/get',
+    "https://" + Phone + "/api/v1/mgmt/config/get",
     headers=headers,
     json=json_data,
     verify=False,
